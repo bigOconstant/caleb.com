bf9babef26f7a58128d9c23b644bb4dd60d4bafe
Caleb McCarthy <caleb.andrew.mccarthy@ibm.com>
2019-02-12 09:27:17 -0600

    Never gonna give you up

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Loading")
+
+function showem(){
+    document.getElementById("weep").innerHTML = "Weep and dispair all other Caleb McCarthy's. I will never relinquish this domain."
+    document.getElementById("wrapper").remove();
+}
