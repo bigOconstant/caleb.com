7a0e8ca3ad8883b2f83da2374477110aa172b1dc
Caleb McCarthy <caleb.a.mccarthy@gmail.com>
2019-02-14 07:12:35 -0600

    Carving out a header

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -3,14 +3,17 @@ function showem(){
     
     document.getElementById("wrapper").remove();
     loadel("views/weep.html","container");
+    loadel("views/header.html","header");
     
 }
 
 function loadel(path,id) {
    fetch(path)
-  .then((response) => response.text())
+	.then((response) => response.text())
   .then(function(html) {
       document.getElementById(id).innerHTML = html;
+      console.log("Html")
+      console.log(html);
   }).catch((error)=>{ console.log(error)});
 
 }
