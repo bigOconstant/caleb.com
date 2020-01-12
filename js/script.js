3d50d43b0df0916937b416f6ad82e4ffe8e06c19
unknown <camccar@g.clemson.edu>
2020-01-12 11:52:38 -0600

    Remove single page stuff

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 function showem(){
     
     document.getElementById("wrapper").remove();
-    loadel("views/weep.html","container");
+    loadel("views/about.html","container");
     
 }
 
@@ -11,51 +11,19 @@ function laughem(){
     buttonel.innerHTML = "<p align='center' style='color:#e0e0e0'> You can't</p>";
 }
 
-function loadel(path,id) {
-   fetch(path)
-	.then((response) => response.text())
-  .then(function(html) {
-      document.getElementById(id).innerHTML = html;
-  }).catch((error)=>{ console.log(error)});
-
-}
-function loadinitialel(path,id){
-    return fetch(path);
-}
-
 function dropDownNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-function navSwitch(page){
-
-    switch(page){
-    case "contact":
-        loadel("views/contact.html","container");
-	    break;
-    case "about":
-	    loadel("views/weep.html","container");
-        break;
-    case "home":
-        loadel("views/home.html","container");
-        break;
-    
-    case "projects" :
-        loadel("views/projects.html","container");
-        break;
-    
-    case "blog":
-	loadel("views/blog.html","container");
-	break;
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
     }
-    switchActive(page);
-    
-}
+  }
+
+
+
+
+
 
 function switchActive(page) {
     window.localStorage.setItem("page",page);
