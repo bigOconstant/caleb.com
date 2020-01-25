44ba5f9110443c7e654a949268c153399ebbdf72
unknown <camccar@g.clemson.edu>
2020-01-25 07:17:41 -0600

    Switch to generated site

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 function showem(){
     
     document.getElementById("wrapper").remove();
-    loadel("views/about.html","container");
+    loadel("views/weep.html","container");
     
 }
 
@@ -11,19 +11,48 @@ function laughem(){
     buttonel.innerHTML = "<p align='center' style='color:#e0e0e0'> You can't</p>";
 }
 
+function loadel(path,id) {
+   fetch(path)
+	.then((response) => response.text())
+  .then(function(html) {
+      document.getElementById(id).innerHTML = html;
+  }).catch((error)=>{ console.log(error)});
+
+}
+function loadinitialel(path,id){
+    return fetch(path);
+}
+
 function dropDownNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
+function navSwitch(page){
 
+    switch(page){
+    case "contact":
+        loadel("views/contact.html","container");
+	    break;
+    case "about":
+	    loadel("views/weep.html","container");
+        break;
+    case "home":
+        loadel("views/home.html","container");
+        break;
+    
+    case "projects" :
+        loadel("views/projects.html","container");
+        break;
+    }
 
-
-
+    switchActive(page);
+    
+}
 
 function switchActive(page) {
     window.localStorage.setItem("page",page);
@@ -39,25 +68,5 @@ function switchActive(page) {
 	    child.classList.add("active");
 	}
     }
-
-    console.log("page = "+getFragment());
     
-}
-
-function getFragment() {
-    var fragment = '';
-    if(this.mode === 'history') {
-        fragment = this.clearSlashes(decodeURI(location.pathname + location.search));
-        fragment = fragment.replace(/\?(.*)$/, '');
-        fragment = this.root != '/' ? fragment.replace(this.root, '') : fragment;
-    } else {
-        var match = window.location.href.match(/#(.*)$/);
-        fragment = match ? match[1] : '';
-    }
-    return clearSlashes(fragment);
-}
-
-
-function clearSlashes(path) {
-    return path.toString().replace(/\/$/, '').replace(/^\//, '');
 }
